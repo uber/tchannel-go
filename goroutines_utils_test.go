d3be65e0a31038ea39af9489b025b6e1dc2ef054
Prashant Varanasi <prashant@uber.com>
2015-11-10 14:31:15 -0800

    Retry goroutine verification a couple of times
    TestWriteAfterTimeout was flaky as a goroutine was not yet updated
    to be runnable, even though it was about to be updated.
    We should not rely on the state of all goroutines to be consistent
    (e.g. one goroutine may have just completed before one become runnable)
    
    We should retry on leaked frames to avoid flakiness.

--- goroutines_utils_test.go
+++ goroutines_utils_test.go
@@ -149,6 +149,7 @@ func VerifyNoBlockedGoroutines(t *testing.T) {
 	}
 	const maxAttempts = 10
 
+	var leakAttempts int
 retry:
 	for i := 0; i < maxAttempts; i++ {
 		// Ignore the first stack which is the current goroutine.
@@ -165,6 +166,13 @@ retry:
 
 		// There are no running/runnable goroutines, so check for bad leaks.
 		leakStacks := getLeakStacks(stacks)
+
+		// If there are leaks found, retry 3 times since the goroutine's state
+		// may not yet have updated.
+		for ; len(leakStacks) > 0 && leakAttempts < 3; leakAttempts++ {
+			i--
+			continue retry
+		}
 		for _, v := range leakStacks {
 			t.Errorf("Found leaked goroutine in state %q Full stack:\n%s\n",
 				v.goState, v.fullStack.String())
