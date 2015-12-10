525bc8bd1fa49ac4975926d26b5d9797bb4e710c
Prashant Varanasi <prashant@uber.com>
2015-12-10 14:25:40 -0800

    Use mockhyperbahn to implement Discover tests

--- hyperbahn/discover_test.go
+++ hyperbahn/discover_test.go
@@ -18,98 +18,52 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-package hyperbahn
+package hyperbahn_test
 
 import (
-	"errors"
 	"testing"
 
-	"github.com/stretchr/testify/mock"
-	"github.com/stretchr/testify/suite"
-	"github.com/uber/tchannel-go"
-	"github.com/uber/tchannel-go/hyperbahn/gen-go/hyperbahn"
-	"github.com/uber/tchannel-go/hyperbahn/mocks"
+	. "github.com/uber/tchannel-go/hyperbahn"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/uber/tchannel-go/testutils"
+	"github.com/uber/tchannel-go/testutils/mockhyperbahn"
 )
 
-// testServicePeerStrings is a static list of peers used in the tests. These
-// match what are returned by createTestServicePeers
-var testServicePeerStrings []string = []string{
-	"127.0.0.1:8000",
-	"127.0.0.2:8001",
-	"1.2.4.8:21150",
-	"10.254.254.1:21151",
+func withSetup(t *testing.T, f func(mh *mockhyperbahn.Mock, client *Client)) {
+	mh, err := mockhyperbahn.New()
+	require.NoError(t, err, "mockhyperbahn.New failed")
+	defer mh.Close()
+
+	clientCh := testutils.NewClient(t, nil)
+	client, err := NewClient(clientCh, mh.Configuration(), nil)
+	require.NoError(t, err, "NewClient failed")
+	defer clientCh.Close()
+
+	f(mh, client)
 }
 
-// testServicePeers is a static list of hyperbahn.ServicePeers used in the
-// tests. They match the testServicePeerStrings.
-var testServicePeers []*hyperbahn.ServicePeer = func() []*hyperbahn.ServicePeer {
-	ips := []int32{
-		0x7f000001, // 127.0.0.1
-		0x7f000002, // 127.0.0.2
-		0x01020408, // 1.2.4.8
-		0x0afefe01, // 10.254.254.1
+func TestDiscoverSuccess(t *testing.T) {
+	peers := []string{
+		"127.0.0.1:8000",
+		"127.0.0.2:8001",
+		"1.2.4.8:21150",
+		"10.254.254.1:21151",
 	}
-	peers := []hyperbahn.ServicePeer{
-		{&hyperbahn.IpAddress{&ips[0]}, 8000},
-		{&hyperbahn.IpAddress{&ips[1]}, 8001},
-		{&hyperbahn.IpAddress{&ips[2]}, 21150},
-		{&hyperbahn.IpAddress{&ips[3]}, 21151},
-	}
-	peerList := []*hyperbahn.ServicePeer{
-		&peers[0],
-		&peers[1],
-		&peers[2],
-		&peers[3],
-	}
-	return peerList
-}()
 
-type DiscoverTests struct {
-	suite.Suite
-	channel *tchannel.Channel
-	client  *Client
-	mock    *mocks.TChanHyperbahn
+	withSetup(t, func(mh *mockhyperbahn.Mock, client *Client) {
+		mh.SetDiscoverResult("test-discover", peers)
+
+		gotPeers, err := client.Discover("test-discover")
+		assert.NoError(t, err, "Discover failed")
+		assert.Equal(t, peers, gotPeers)
+	})
 }
 
-func (s *DiscoverTests) SetupTest() {
-	var err error
-
-	s.channel, err = tchannel.NewChannel("test", nil)
-	s.NoError(err)
-
-	err = s.channel.ListenAndServe("127.0.0.1:0")
-	s.NoError(err)
-
-	s.client, err = NewClient(s.channel, Configuration{
-		InitialNodes: []string{s.channel.PeerInfo().HostPort},
-	}, nil)
-	s.NoError(err)
-
-	s.mock = &mocks.TChanHyperbahn{}
-	s.client.hyperbahnClient = s.mock
-}
-
-func (s *DiscoverTests) TearDownTest() {
-	s.channel.Close()
-	s.client.Close()
-}
-
-func (s *DiscoverTests) TestDiscoverSuccess() {
-	s.mock.On("Discover", mock.Anything, mock.Anything).Return(&hyperbahn.DiscoveryResult_{Peers: testServicePeers}, nil)
-
-	peers, err := s.client.Discover("test")
-	s.NoError(err)
-	s.Equal(testServicePeerStrings, peers)
-}
-
-func (s *DiscoverTests) TestDiscoverFails() {
-	s.mock.On("Discover", mock.Anything, mock.Anything).Return(nil, errors.New("testing error"))
-
-	peers, err := s.client.Discover("test")
-	s.Error(err)
-	s.Nil(peers)
-}
-
-func TestDiscoverTests(t *testing.T) {
-	suite.Run(t, new(DiscoverTests))
+func TestDiscoverFails(t *testing.T) {
+	withSetup(t, func(mh *mockhyperbahn.Mock, client *Client) {
+		_, err := client.Discover("test-discover")
+		assert.Error(t, err, "Discover should fail due to no discover result set in the mock")
+	})
 }
