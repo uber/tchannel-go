1a1cc6a4296f0ce1fe9b604d8bfc1c52d8fe04b6
Prashant Varanasi <prashant@uber.com>
2015-12-23 16:28:44 -0800

    Add logging for connection close

--- connection.go
+++ connection.go
@@ -794,6 +794,7 @@ func (c *Connection) closeNetwork() {
 	// NB(mmihic): The sender goroutine will exit once the connection is
 	// closed; no need to close the send channel (and closing the send
 	// channel would be dangerous since other goroutine might be sending)
+	c.log.Debugf("Closing underlying network connection")
 	atomic.AddInt32(&c.closeNetworkCalled, 1)
 	if err := c.conn.Close(); err != nil {
 		c.log.Warnf("could not close connection to peer %s: %v", c.remotePeerInfo, err)
