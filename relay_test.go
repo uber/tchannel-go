603458dcda3054315471cc581402293a187bb883
Akshay Shah <shah@uber.com>
2016-02-04 18:13:50 -0800

    Fix relay test

--- relay_test.go
+++ relay_test.go
@@ -18,31 +18,35 @@ func TestRelay(t *testing.T) {
 	})
 	require.NoError(t, err, "Failed to create a relay channel.")
 	require.NoError(t, relay.ListenAndServe("127.0.0.1:0"), "Relay failed to listen.")
-	defer relay.Close()
 
 	data := []byte("fake-body")
 
 	server := testutils.NewServer(t, testutils.NewOpts().SetServiceName("test"))
-	defer server.Close()
 	server.Register(raw.Wrap(newTestHandler(t)), "echo")
 	relay.RelayHosts().(*SimpleRelayHosts).Add("test", server.PeerInfo().HostPort)
 
 	client := testutils.NewClient(t, nil)
 	client.Peers().Add(relay.PeerInfo().HostPort)
-	defer client.Close()
 
 	var wg sync.WaitGroup
+	wg.Add(1)
 
-	defer func(c *Channel) {
+	go func(c *Channel) {
 		sc := client.GetSubChannel("test")
 		ctx, cancel := NewContext(time.Second)
 		defer cancel()
 
 		arg2, arg3, _, err := raw.CallSC(ctx, sc, "echo", []byte("fake-header"), data)
+		t.Log(string(arg3))
 		require.NoError(t, err, "Relayed call failed.")
 		assert.Equal(t, "fake-header", string(arg2), "Header was mangled during relay.")
 		assert.Equal(t, "fake-body", string(arg3), "Body was mangled during relay.")
+
+		wg.Done()
 	}(client)
 
 	wg.Wait()
+	client.Close()
+	relay.Close()
+	server.Close()
 }
