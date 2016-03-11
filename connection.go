66fef1b159fe4f18aaa14fc2e7a713f4ee26e114
Akshay Shah <shah@uber.com>
2016-03-11 10:58:39 -0800

    Small cleanups from CR

--- connection.go
+++ connection.go
@@ -789,10 +789,8 @@ func (c *Connection) checkExchanges() {
 
 	var updated connectionState
 	if c.readState() == connectionStartClose {
-		if c.relay != nil {
-			if !c.relay.canClose() {
-				return
-			}
+		if !c.relay.canClose() {
+			return
 		}
 		if c.inbound.count() == 0 && moveState(connectionStartClose, connectionInboundClosed) {
 			updated = connectionInboundClosed
