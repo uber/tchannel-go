1e1f75e9d38dd85e3aacda4d09a6f9861c8edbce
Akshay Shah <shah@uber.com>
2016-04-22 11:11:05 -0700

    Interpose a relay in frame pool tests

--- frame_pool_test.go
+++ frame_pool_test.go
@@ -103,7 +103,9 @@ func TestFramesReleased(t *testing.T) {
 	opts := testutils.NewOpts().
 		SetServiceName("swap-server").
 		SetFramePool(pool).
-		AddLogFilter("Couldn't find handler.", numGoroutines*requestsPerGoroutine)
+		AddLogFilter("Couldn't find handler.", 2*numGoroutines*requestsPerGoroutine).
+		SetRelay()
+
 	testutils.WithTestServer(t, opts, func(ts *testutils.TestServer) {
 		ts.Register(raw.Wrap(&swapper{t}), "swap")
 
@@ -159,7 +161,9 @@ func TestDirtyFrameRequests(t *testing.T) {
 
 	opts := testutils.NewOpts().
 		SetServiceName("swap-server").
-		SetFramePool(dirtyFramePool{})
+		SetFramePool(dirtyFramePool{}).
+		SetRelay()
+
 	testutils.WithTestServer(t, opts, func(ts *testutils.TestServer) {
 		ts.Register(raw.Wrap(&swapper{t}), "swap")
 
