85e63d6fec845f9f6eb3b366e32c08ab18694230
Yuri Shkuro <ys@uber.com>
2016-08-09 15:57:26 -0400

    Do not install docker, only docker-compose

--- crossdock/rules.mk
+++ crossdock/rules.mk
@@ -27,12 +27,9 @@ crossdock-logs:
 .PHONY: install_docker_ci
 install_docker_ci:
 ifdef SHOULD_XDOCK
-	@echo "Installing Docker $${DOCKER_VERSION:?'DOCKER_VERSION env not set'}"
-	apt-cache madison docker-engine
-	sudo apt-get -o Dpkg::Options::="--force-confnew" install -y docker-engine=$${DOCKER_VERSION}
 	docker version
 	@echo "Installing docker-compose $${DOCKER_COMPOSE_VERSION:?'DOCKER_COMPOSE_VERSION env not set'}"
-	sudo rm /usr/local/bin/docker-compose
+	sudo rm -f /usr/local/bin/docker-compose
 	curl -L https://github.com/docker/compose/releases/download/$${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
 	chmod +x docker-compose
 	sudo mv docker-compose /usr/local/bin
