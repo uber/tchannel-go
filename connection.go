94acfc820ea807d6d28c5d06ea371ae7e8d09d67
Prashant Varanasi <prashant@uber.com>
2016-11-14 16:46:21 -0800

    Address code review comments

--- connection.go
+++ connection.go
@@ -309,7 +309,7 @@ func (ch *Channel) newConnection(conn net.Conn, outboundHP string, initialState 
 	c.inbound.onAdded = c.onExchangeAdded
 	c.outbound.onAdded = c.onExchangeAdded
 
-	if ch.relayHost != nil {
+	if ch.RelayHost() != nil {
 		c.relay = NewRelayer(ch, c)
 	}
 
