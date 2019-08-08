4203bf3aa2e508e006b1af2a133ceb65dfc4e1f4
Chien-Chih Liao <ccliao@uber.com>
2019-08-08 11:13:44 -0700

    Use offset check to ensure we don't access out-of-range buffer space.

--- thrift/arg2/kv_iterator.go
+++ thrift/arg2/kv_iterator.go
@@ -7,32 +7,37 @@ import "encoding/binary"
 // NOTE: to be optimized for performance, we try to limit the allocation
 // done in the process of iteration.
 type KeyValIterator struct {
+	arg2Len               int
 	leftPairCount         int
-	start                 []byte
 	keyOffset, keyLen     int
 	valueOffset, valueLen int
+	arg2Payload           []byte
 }
 
 // InitKeyValIterator inits a KeyValIterator with the buffer pointing at
 // start of Arg2.
 // NOTE: tchannel-thrift Arg Scheme starts with number of key/value pair.
-func InitKeyValIterator(arg2Start []byte) (KeyValIterator, bool) {
-	nh := int(binary.BigEndian.Uint16(arg2Start[0:2]))
+func InitKeyValIterator(arg2Payload []byte, arg2Len int) (KeyValIterator, bool) {
+	nh := int(binary.BigEndian.Uint16(arg2Payload[0:2]))
 	if nh <= 0 {
 		return KeyValIterator{}, false
 	}
 
-	return createKVIterator(nh, arg2Start[2:]), true
+	return KeyValIterator{
+		leftPairCount: nh,
+		arg2Len:       arg2Len,
+		arg2Payload:   arg2Payload,
+	}.next(2 /*nh*/)
 }
 
 // Key Returns the key
 func (i KeyValIterator) Key() []byte {
-	return i.start[i.keyOffset : i.keyOffset+i.keyLen]
+	return i.arg2Payload[i.keyOffset : i.keyOffset+i.keyLen]
 }
 
 // Value returns value.
 func (i KeyValIterator) Value() []byte {
-	return i.start[i.valueOffset : i.valueOffset+i.valueLen]
+	return i.arg2Payload[i.valueOffset : i.valueOffset+i.valueLen]
 }
 
 // Next returns next iterator. Return nil if no more key/value pair available.
@@ -41,29 +46,38 @@ func (i KeyValIterator) Next() (KeyValIterator, bool) {
 		return KeyValIterator{}, false
 	}
 
-	return createKVIterator(
-		i.leftPairCount,
-		i.start[2 /*uint16 len*/ +i.keyLen+2 /*uint16 len*/ +i.valueLen:],
-	), true
+	return i.next(i.valueOffset + i.valueLen)
 }
 
-func createKVIterator(pairCount int, bufferStart []byte) KeyValIterator {
-	var cur int
-	keyLen := int(binary.BigEndian.Uint16(bufferStart[cur : cur+2]))
+// cur is the offset from the start of Arg2 payload to next key/value pair
+// we want to iterate to.
+func (i KeyValIterator) next(cur int) (KeyValIterator, bool) {
+	if cur+2 > i.arg2Len {
+		return KeyValIterator{}, false
+	}
+	keyLen := int(binary.BigEndian.Uint16(i.arg2Payload[cur : cur+2]))
 	cur += 2
 	keyOffset := cur
 	cur += keyLen
 
-	valueLen := int(binary.BigEndian.Uint16(bufferStart[cur : cur+2]))
+	if cur+2 > i.arg2Len {
+		return KeyValIterator{}, false
+	}
+	valueLen := int(binary.BigEndian.Uint16(i.arg2Payload[cur : cur+2]))
 	cur += 2
 	valueOffset := cur
 
+	if valueOffset+valueLen > i.arg2Len {
+		return KeyValIterator{}, false
+	}
+
 	return KeyValIterator{
-		start:         bufferStart,
-		leftPairCount: pairCount - 1,
+		arg2Len:       i.arg2Len,
+		arg2Payload:   i.arg2Payload,
+		leftPairCount: i.leftPairCount - 1,
 		keyOffset:     keyOffset,
 		keyLen:        keyLen,
 		valueOffset:   valueOffset,
 		valueLen:      valueLen,
-	}
+	}, true
 }
--- thrift/arg2/kv_iterator_test.go
+++ thrift/arg2/kv_iterator_test.go
@@ -22,7 +22,7 @@ func TestKeyValIterator(t *testing.T) {
 		wb.WriteLen16String(fmt.Sprintf("value%v", i))
 	}
 
-	iter, ok := InitKeyValIterator(buf)
+	iter, ok := InitKeyValIterator(buf, wb.BytesWritten())
 	for i := 0; i < nh; i++ {
 		assert.True(t, ok)
 		assert.Equal(t, fmt.Sprintf("key%v", i), string(iter.Key()))
@@ -35,7 +35,53 @@ func TestKeyValIterator(t *testing.T) {
 		buf := make([]byte, 2)
 		wb := typed.NewWriteBuffer(buf)
 		wb.WriteUint16(0)
-		_, ok := InitKeyValIterator(buf)
+		_, ok := InitKeyValIterator(buf, wb.BytesWritten())
 		assert.False(t, ok)
+	})
+
+	t.Run("bad key value length", func(t *testing.T) {
+		buf := make([]byte, testBufSize)
+		wb := typed.NewWriteBuffer(buf)
+		wb.WriteUint16(1)
+		wb.WriteLen16String("key")
+		wb.WriteLen16String("value")
+		tests := []struct {
+			msg          string
+			arg2Len      int
+			wantIterator bool
+		}{
+			{
+				msg:          "ok",
+				arg2Len:      wb.BytesWritten(),
+				wantIterator: true,
+			},
+			{
+				msg:          "not enough to read key len",
+				arg2Len:      3, // nh (2) + 1
+				wantIterator: false,
+			},
+			{
+				msg:          "not enough to read value len",
+				arg2Len:      8, // nh (2) + 2 + len(key) + 1
+				wantIterator: false,
+			},
+			{
+				msg:          "not enough to iterate key",
+				arg2Len:      13, // nh (2) + 2 + len(key) + 2 + len(value) = 14
+				wantIterator: false,
+			},
+		}
+
+		for _, tt := range tests {
+			t.Run(tt.msg, func(t *testing.T) {
+				iter, ok := InitKeyValIterator(buf, tt.arg2Len)
+				assert.Equal(t, tt.wantIterator, ok)
+
+				if tt.wantIterator {
+					assert.Equal(t, "key", string(iter.Key()))
+					assert.Equal(t, "value", string(iter.Value()))
+				}
+			})
+		}
 	})
 }
