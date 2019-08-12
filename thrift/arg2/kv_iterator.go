7cd873c23a3b2616b0aee4491f23da1c554f5a3a
Chien-Chih Liao <ccliao@uber.com>
2019-08-12 15:14:48 -0700

    CR: optimizations to reduce vars; update test; update package doc

--- thrift/arg2/kv_iterator.go
+++ thrift/arg2/kv_iterator.go
@@ -1,3 +1,7 @@
+// Package arg2 contains tchannel thrift Arg2 interfaces for external use.
+//
+// These interfaces are currently unstable, and aren't covered by the API
+// backwards-compatibility guarantee.
 package arg2
 
 import "encoding/binary"
@@ -7,18 +11,17 @@ import "encoding/binary"
 // NOTE: to be optimized for performance, we try to limit the allocation
 // done in the process of iteration.
 type KeyValIterator struct {
-	arg2Len               int
-	leftPairCount         int
-	keyOffset, keyLen     int
-	valueOffset, valueLen int
 	arg2Payload           []byte
+	leftPairCount         int
+	keyOffset             int
+	valueOffset, valueLen int
 }
 
 // InitKeyValIterator inits a KeyValIterator with the buffer pointing at
 // start of Arg2.
 // NOTE: tchannel-thrift Arg Scheme starts with number of key/value pair.
-func InitKeyValIterator(arg2Payload []byte, arg2Len int) (KeyValIterator, bool) {
-	if arg2Len < 2 {
+func InitKeyValIterator(arg2Payload []byte) (KeyValIterator, bool) {
+	if len(arg2Payload) < 2 {
 		return KeyValIterator{}, false
 	}
 
@@ -29,14 +32,13 @@ func InitKeyValIterator(arg2Payload []byte, arg2Len int) (KeyValIterator, bool) 
 
 	return KeyValIterator{
 		leftPairCount: nh,
-		arg2Len:       arg2Len,
 		arg2Payload:   arg2Payload,
 	}.next(2 /*nh*/)
 }
 
 // Key Returns the key
 func (i KeyValIterator) Key() []byte {
-	return i.arg2Payload[i.keyOffset : i.keyOffset+i.keyLen]
+	return i.arg2Payload[i.keyOffset : i.valueOffset-2 /*2B length*/]
 }
 
 // Value returns value.
@@ -56,7 +58,8 @@ func (i KeyValIterator) Next() (KeyValIterator, bool) {
 // cur is the offset from the start of Arg2 payload to next key/value pair
 // we want to iterate to.
 func (i KeyValIterator) next(cur int) (KeyValIterator, bool) {
-	if cur+2 > i.arg2Len {
+	arg2Len := len(i.arg2Payload)
+	if cur+2 > arg2Len {
 		return KeyValIterator{}, false
 	}
 	keyLen := int(binary.BigEndian.Uint16(i.arg2Payload[cur : cur+2]))
@@ -64,23 +67,21 @@ func (i KeyValIterator) next(cur int) (KeyValIterator, bool) {
 	keyOffset := cur
 	cur += keyLen
 
-	if cur+2 > i.arg2Len {
+	if cur+2 > arg2Len {
 		return KeyValIterator{}, false
 	}
 	valueLen := int(binary.BigEndian.Uint16(i.arg2Payload[cur : cur+2]))
 	cur += 2
 	valueOffset := cur
 
-	if valueOffset+valueLen > i.arg2Len {
+	if valueOffset+valueLen > arg2Len {
 		return KeyValIterator{}, false
 	}
 
 	return KeyValIterator{
-		arg2Len:       i.arg2Len,
 		arg2Payload:   i.arg2Payload,
 		leftPairCount: i.leftPairCount - 1,
 		keyOffset:     keyOffset,
-		keyLen:        keyLen,
 		valueOffset:   valueOffset,
 		valueLen:      valueLen,
 	}, true
