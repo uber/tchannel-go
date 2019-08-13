0b6c738922542d08ae954a04291f4c34b53b239c
Chien-Chih Liao <ccliao@uber.com>
2019-08-13 11:33:39 -0700

    update name in test table

--- thrift/arg2/kv_iterator_test.go
+++ thrift/arg2/kv_iterator_test.go
@@ -54,35 +54,35 @@ func TestKeyValIterator(t *testing.T) {
 		wb.WriteLen16String("key")
 		wb.WriteLen16String("value")
 		tests := []struct {
-			msg          string
-			arg2Len      int
-			wantIterator string
+			msg     string
+			arg2Len int
+			wantErr string
 		}{
 			{
 				msg:     "ok",
 				arg2Len: wb.BytesWritten(),
 			},
 			{
-				msg:          "not enough to read key len",
-				arg2Len:      3, // nh (2) + 1
-				wantIterator: "invalid key offset 2 (arg2 len 3)",
+				msg:     "not enough to read key len",
+				arg2Len: 3, // nh (2) + 1
+				wantErr: "invalid key offset 2 (arg2 len 3)",
 			},
 			{
-				msg:          "not enough to read value len",
-				arg2Len:      8, // nh (2) + 2 + len(key) + 1
-				wantIterator: "invalid value offset 7 (key offset 4, key len 3, arg2 len 8)",
+				msg:     "not enough to read value len",
+				arg2Len: 8, // nh (2) + 2 + len(key) + 1
+				wantErr: "invalid value offset 7 (key offset 4, key len 3, arg2 len 8)",
 			},
 			{
-				msg:          "not enough to iterate value",
-				arg2Len:      13, // nh (2) + 2 + len(key) + 2 + len(value) = 14
-				wantIterator: "value exceeds arg2 range (offset 9, len 5, arg2 len 13)",
+				msg:     "not enough to iterate value",
+				arg2Len: 13, // nh (2) + 2 + len(key) + 2 + len(value) = 14
+				wantErr: "value exceeds arg2 range (offset 9, len 5, arg2 len 13)",
 			},
 		}
 
 		for _, tt := range tests {
 			t.Run(tt.msg, func(t *testing.T) {
 				iter, err := NewKeyValIterator(buf[:tt.arg2Len])
-				if tt.wantIterator == "" {
+				if tt.wantErr == "" {
 					assert.NoError(t, err)
 					assert.Equal(t, "key", string(iter.Key()))
 					assert.Equal(t, "value", string(iter.Value()))
@@ -90,7 +90,7 @@ func TestKeyValIterator(t *testing.T) {
 				}
 
 				require.Error(t, err)
-				assert.Contains(t, err.Error(), tt.wantIterator)
+				assert.Contains(t, err.Error(), tt.wantErr)
 			})
 		}
 	})
