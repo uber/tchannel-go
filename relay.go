3d0c9c321cc058770ad3b778479444a1fb09941b
Prashant Varanasi <prashant@uber.com>
2020-03-27 12:26:50 -0700

    relay: Include time since last activity in dropped call log (#774)

--- relay.go
+++ relay.go
@@ -285,12 +285,17 @@ func (r *Relayer) Receive(f *Frame, fType frameType) (sent bool, failureReason s
 		// Since this is typically due to the send buffer being full, get send buffer
 		// usage + limit and add that to the log.
 		sendBuf, sendBufLimit, sendBufErr := r.conn.sendBufSize()
+		now := r.conn.timeNow().UnixNano()
 		logFields := []LogField{
 			{"id", id},
 			{"destConnSendBufferCurrent", sendBuf},
 			{"destConnSendBufferLimit", sendBufLimit},
 			{"sendChQueued", len(r.conn.sendCh)},
 			{"sendChCapacity", cap(r.conn.sendCh)},
+			{"lastActivityRead", r.conn.lastActivityRead.Load()},
+			{"lastActivityWrite", r.conn.lastActivityRead.Load()},
+			{"sinceLastActivityRead", time.Duration(now - r.conn.lastActivityRead.Load()).String()},
+			{"sinceLastActivityWrite", time.Duration(now - r.conn.lastActivityWrite.Load()).String()},
 		}
 		if sendBufErr != nil {
 			logFields = append(logFields, LogField{"destConnSendBufferError", sendBufErr.Error()})
